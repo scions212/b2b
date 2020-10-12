afde71b0e186d333a156980a79002a4513c7d495
Andres Rodriguez <arodriguez_212@hotmail.com>
2020-10-12 18:00:27 -0500

    B2B login listo

--- AngularB2B/src/app/app.component.ts
+++ AngularB2B/src/app/app.component.ts
@@ -13,18 +13,13 @@ import {Observable } from 'rxjs';
 export class AppComponent  implements OnInit { 
   title = 'AngularB2B';
   public isLogged=false;
+
   public user$:Observable<any> = this.authSvc.AfAuth.user;
-  public user :any;
+  
 
   constructor( private authSvc:AuthService, private router:Router) {}
     async ngOnInit(){
-     console.log('Navbar');
-      this.user =await this.authSvc.getCurrentUser();
-      if (this.user){
-        this.isLogged=true;
-         console.log('user->', this.user);
-         this.router.navigate(['/home'])
-      }
+   
     }
     async onLogout(){
 
