3472e7fdb8fb2d50baf1ed4d5afb05fca2a50949
Andres Rodriguez <arodriguez_212@hotmail.com>
2020-11-10 23:15:39 -0500

    B2B

--- backendb2b/routes/mensaje.js
+++ backendb2b/routes/mensaje.js
@@ -14,8 +14,8 @@ var md_upload = multipart({ uploadDir: './uploads/file'});
 
 
 router.post('/POST_MESSAGE/:groupId', md_auth.authenticated,MensajeController.addMssg);
-/*router.put('/PUT_MESSAGE/:messageeId', md_auth.authenticated,MensajeController.updateMssg);
 router.delete('/DELETE_MESSAGE/:groupId/:messageId', md_auth.authenticated,MensajeController.deleteMssg);
+/*router.put('/PUT_MESSAGE/:messageeId', md_auth.authenticated,MensajeController.updateMssg);
 router.get('/GET_SEARCH_MESSAGE/:search',md_auth.authenticated, MensajeController.searchMssg);
 router.post('/UploadProfile/',[md_upload,md_auth.authenticated], MensajeController.uploadPhotoProfile);
 router.get('/photoProfile/:fileName', MensajeController.photoProfile);
